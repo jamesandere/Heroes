4b7379fa01d76eb07b0c9030807476987028942f
jamesandere <jamesandere56@gmail.com>
2019-04-12 16:46:31 +0300

    Create functions to get name and age

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private String otherName;
+    private int otherAge;
+    private int id;
+    private String otherPowers;
+    private String otherWeakness;
+    private static List<Hero> instances = new ArrayList<>();
+
+    public Hero(String name, int age, String power, String weakness) {
+        otherName = name;
+        otherAge = age;
+        otherPowers = power;
+        otherWeakness = weakness;
+        instances.add(this);
+        id = instances.size();
+    }
+
+    public String getOtherName() {
+        return otherName;
+    }
+
+    public int getOtherAge() {
+        return otherAge;
+    }
 }
