68bae5514c19b598a7935db5bd281a3649c1cfa2
jamesandere <jamesandere56@gmail.com>
2019-04-13 11:57:28 +0300

    Create functions for squad

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Squad {
+ private String squadName;
+ private int id;
+ private int squadSize;
+ private String squadMission;
+ public static List<Squad> instances = new ArrayList<>();
+
+ public Squad (String name, int size, String mission){
+     squadName = name;
+     squadSize = size;
+     squadMission = mission;
+     instances.add(this);
+     id = instances.size();
+ }
+
+ public String getSquadName(){
+     return squadName;
+ }
+
+ public int getSquadSize(){
+     return squadSize;
+ }
+
+    public String getSquadMission() {
+        return squadMission;
+    }
 }
