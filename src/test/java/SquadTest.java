4010f2a0eb9ee3d82616136596c94b77ec1b280f
jamesandere <jamesandere56@gmail.com>
2019-04-13 11:58:29 +0300

    Create tests for squad

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,1 +1,22 @@
+import org.junit.*;
+import static org.junit.Assert.*;
 
+public class SquadTest {
+  @Test
+  public void Squad_gets_name(){
+    Squad squad = new Squad("GangGang", 5, "Short people matter");
+    assertEquals("GangGang", squad.getSquadName());
+  }
+
+  @Test
+  public void Squad_gets_size(){
+    Squad squad = new Squad("GangGang", 5, "Short people matter");
+    assertEquals(5, squad.getSquadSize());
+  }
+
+  @Test
+  public void Squad_gets_mission(){
+    Squad squad = new Squad("GangGang", 5, "Short people matter");
+    assertEquals("Short people matter", squad.getSquadMission());
+  }
+}
